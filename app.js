ee20e10a74fd291276dd95d837e225c955596432
Antonio Gabriel Garcez <138391757+GaabGarcez@users.noreply.github.com>
2024-01-14 10:03:18 -0300

    Versão 7.0

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const { WebSocketServer } = require('ws');
+const { WebSocketServer, WebSocket } = require('ws'); // Importação adicional aqui
+const http = require('http');
 const app = express();
+const server = http.createServer(app);
 const wss = new WebSocketServer({ noServer: true });
 const clients = {};
 
@@ -10,19 +12,24 @@ app.post('/request', async (req, res) => {
     const { uuid_user, mensagem } = req.body;
     const client = clients[uuid_user];
 
-    if (client && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify({ mensagem }));
-
-        // Aguarda resposta do cliente
-        const resposta = await aguardarResposta(client);
-        res.send(resposta);
+    if (client && client.readyState === WebSocket.OPEN) { // Usando WebSocket.OPEN
+        try {
+            client.send(JSON.stringify({ mensagem }));
+            const resposta = await aguardarResposta(client);
+            res.send(resposta);
+        } catch (error) {
+            console.error("Erro ao enviar mensagem para o cliente:", error);
+            delete clients[uuid_user];
+            res.send("O Dexter não está ativo");
+        }
     } else {
+        delete clients[uuid_user];
         res.send("O Dexter não está ativo");
     }
 });
+
 wss.on('connection', (ws, req) => {
     const uuid = req.url.split('/connection/')[1]; // Extrai o UUID da URL
-    clients[uuid] = ws;
     if (clients[uuid]) {
         clients[uuid].terminate(); // Fecha a conexão antiga, se existir
     }
@@ -32,7 +39,7 @@ wss.on('connection', (ws, req) => {
         ws.isAlive = true;
     });
 
-    // Restante do código
+    // Restante do código...
 });
 
 // Função para aguardar resposta do cliente
@@ -41,12 +48,19 @@ function aguardarResposta(ws) {
         ws.once('message', (message) => {
             resolve(message);
         });
-        // Implemente um timeout ou lógica de erro, se necessário
     });
 }
 
-const PORT = process.env.PORT || 1000; // Use a porta do ambiente ou 3000 como padrão
+// Ligar o servidor Express ao WebSocketServer
+server.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+const PORT = process.env.PORT || 1000;
+
+// Escutando a porta usando o servidor HTTP
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
