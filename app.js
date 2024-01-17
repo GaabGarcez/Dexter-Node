ac5ed3cc4144376cd18ac5605bdb8271a8a576e6
Antonio Gabriel Garcez <138391757+GaabGarcez@users.noreply.github.com>
2024-01-17 09:53:17 -0300

    Versão 8.2

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocketServer({ noServer: true });
+const { v4: uuidv4 } = require('uuid');
 const clients = {};
+const requisicoesPendentes = {};
 
 app.use(express.json());
 
 app.post('/request', async (req, res) => {
     const { uuid_user, mensagem } = req.body;
     const client = clients[uuid_user];
+    const id_requisicao = uuidv4();  // Gera um UUID único
 
-    if (client && client.readyState === WebSocket.OPEN) { // Usando WebSocket.OPEN
+    if (client && client.readyState === WebSocket.OPEN) {
+        requisicoesPendentes[id_requisicao] = res;  // Armazena a resposta HTTP para enviar depois que a resposta do cliente chegar
         try {
-            client.send(JSON.stringify({ mensagem }));
-            const resposta = await aguardarResposta(client);
-            res.send(resposta);
+            client.send(JSON.stringify({ id: id_requisicao, mensagem }));
         } catch (error) {
             console.error("Erro ao enviar mensagem para o cliente:", error);
             delete clients[uuid_user];
+            delete requisicoesPendentes[id_requisicao]; // Remove a requisição pendente também aqui
             res.send("O Dexter não está em execução no seu servidor. Contate o suporte.");
         }
     } else {
@@ -38,26 +41,15 @@ wss.on('connection', (ws, req) => {
     ws.on('pong', () => {
         ws.isAlive = true;
     });
-
-    // Restante do código...
-});
-
-// Função para aguardar resposta do cliente
-function aguardarResposta(ws, timeoutMs = 35000) {
-    return new Promise((resolve, reject) => {
-        const timer = setTimeout(() => {
-            ws.removeListener('message', onMessage);
-            reject(new Error('O Dexter não está em execução no seu servidor. Contate o suporte.'));
-        }, timeoutMs);
-
-        const onMessage = (message) => {
-            clearTimeout(timer);
-            resolve(message);
-        };
-
-        ws.once('message', onMessage);
+    ws.on('message', (message) => {
+        const resposta = JSON.parse(message);
+        const resHttp = requisicoesPendentes[resposta.id];
+        if (resHttp) {
+            resHttp.send(resposta);
+            delete requisicoesPendentes[resposta.id];  // Limpa a referência depois de enviar a resposta
+        }
     });
-}
+});
 
 // Ligar o servidor Express ao WebSocketServer
 server.on('upgrade', (request, socket, head) => {
